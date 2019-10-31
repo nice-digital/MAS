1ee759993f5b35b11d419521ab196dfd69e48e13
dylan <dylan.riley@nice.org.uk>
2019-10-31 17:07:01 +0000

    MAS-109 Add core fields to Item lamdba

--- lambda/MAS/Models/Item.cs
+++ lambda/MAS/Models/Item.cs
@@ -1,4 +1,6 @@
 ﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 namespace MAS.Models
 {
@@ -7,5 +9,15 @@ namespace MAS.Models
         [JsonProperty("_id")]
         public string Id { get; set; }
         public string Title { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Category { get; set; }
+        public string Source { get; set; }
+        public string GeographicalCoverage { get; set; }
+        public IEnumerable<string> Speciality { get; set; }
+        public string ShortSummary { get; set; }
+        public string ResourceLinks { get; set; }
+        public string UKMiComment { get; set; }
+        public int MAWScore { get; set; }
     }
 }
--- lambda/MAS/Services/ContentService.cs
+++ lambda/MAS/Services/ContentService.cs
@@ -19,7 +19,16 @@ namespace MAS.Services
             using (WebClient client = new WebClient())
             {
                 var jsonStr = await client.DownloadStringTaskAsync(new Uri(AppSettings.CMSConfig.URI + itemId));
-                Item json = JsonConvert.DeserializeObject<Item>(jsonStr);
+                Item json = null;
+                try
+                {
+                    json = JsonConvert.DeserializeObject<Item>(jsonStr);
+                }
+                catch(Exception e)
+                {
+                    throw e;
+                }
+                
                 return json;
             }
         }
