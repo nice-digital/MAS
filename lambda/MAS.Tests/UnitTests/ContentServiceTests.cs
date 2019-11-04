47887686310b3a7625e8e8e24cc534809af6186d
dylan <dylan.riley@nice.org.uk>
2019-11-04 17:21:11 +0000

    MAS-109 Update test to dynamically test data model

--- lambda/MAS.Tests/UnitTests/ContentServiceTests.cs
+++ lambda/MAS.Tests/UnitTests/ContentServiceTests.cs
@@ -1,8 +1,11 @@
 ﻿using Amazon.S3;
 using MAS.Configuration;
+using MAS.Models;
 using MAS.Services;
 using MAS.Tests.Infrastructure;
+using Newtonsoft.Json;
 using Shouldly;
+using System.IO;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -25,8 +28,14 @@ namespace MAS.Tests.UnitTests
             var result = await contentService.GetItemAsync("");
 
             //Assert
-            result.Id.ShouldBe("5daeb5af22565a82530d7373"); 
-            result.Title.ShouldBe("Wonder drug");
+            string actualOutputJson = JsonConvert.SerializeObject(result);
+			//Ensure the json is formatted the same
+            var expectedOutputObject = JsonConvert.DeserializeObject<Item>(File.ReadAllText("C:/_src/MAS/lambda/MAS.Tests/Feeds/single-item.json"));
+            string expectedOutputJson = JsonConvert.SerializeObject(expectedOutputObject);
+
+            actualOutputJson.ShouldBe(expectedOutputJson);
+            
+
         }
     }
 }
