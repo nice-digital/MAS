08ca083bfe134ce811243d8a72275023ac56038b
dylan <dylan.riley@nice.org.uk>
2019-11-07 12:41:30 +0000

    MAS-109 Comment out non-core fields

--- lambda/MAS/Models/Item.cs
+++ lambda/MAS/Models/Item.cs
@@ -11,22 +11,22 @@ namespace MAS.Models
         public string Id { get; set; }
         [Required, JsonRequired]
         public string Title { get; set; }
-        [Required, JsonRequired]
-        public DateTime PublicationDate { get; set; }
-        [Required, JsonRequired]
-        public DateTime CreatedDate { get; set; }
-        [Required, JsonRequired]
-        public string Category { get; set; }
+        //[Required, JsonRequired]
+        //public DateTime PublicationDate { get; set; }
+        //[Required, JsonRequired]
+        //public DateTime CreatedDate { get; set; }
+        //[Required, JsonRequired]
+        //public string Category { get; set; }
         [Required, JsonRequired]
         public string Source { get; set; } //TODO: Needs source item values instead of source item Ids
-        [Required, JsonRequired]
-        public IEnumerable<string> Speciality { get; set; } //TODO: Needs speciality item values instead of Ids
+        //[Required, JsonRequired]
+       // public IEnumerable<string> Speciality { get; set; } //TODO: Needs speciality item values instead of Ids
         [Required, JsonRequired]
         public string ShortSummary { get; set; }
-        public string ResourceLinks { get; set; }
-        [Required, JsonRequired]
-        public string UKMiComment { get; set; }
-        [Required, JsonRequired]
-        public int RelevancyScore { get; set; }
+        //public string ResourceLinks { get; set; }
+        //[Required, JsonRequired]
+        //public string UKMiComment { get; set; }
+        //[Required, JsonRequired]
+        //public int RelevancyScore { get; set; }
     }
 }
