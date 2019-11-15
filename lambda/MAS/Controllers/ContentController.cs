2b209d031f774b435087f2666229ed0e4669b246
dylan <dylan.riley@nice.org.uk>
2019-11-15 12:55:50 +0000

    MAS-109 Fix & Improve test

--- lambda/MAS/Controllers/ContentController.cs
+++ lambda/MAS/Controllers/ContentController.cs
@@ -16,23 +16,20 @@ namespace MAS.Controllers
     [Route("api/[controller]")]
     public class ContentController : ControllerBase
     {
-        private readonly IContentService _contentService;
         private readonly IS3Service _s3Service;
         private readonly ILogger<ContentController> _logger;
         private readonly IAmazonS3 _amazonS3;
 
-        public ContentController(IContentService contentService, IS3Service s3Service, ILogger<ContentController> logger)
+        public ContentController(IS3Service s3Service, ILogger<ContentController> logger)
         {
-            _contentService = contentService;
             _s3Service = s3Service;
             _logger = logger; //TODO: Log response errors
         }
 
-        //PUT api/content/5dac429284dd4afe5eb8fae6
+        //PUT api/content/
         [HttpPut]
-        public async Task<IActionResult> PutAsync([FromBody] string json)
+        public async Task<IActionResult> PutAsync([FromBody] Item item)
         {
-            var item = JsonConvert.DeserializeObject<Item>(json);
             var response = _s3Service.WriteToS3(item);
 
             return Validate(response.Result.HttpStatusCode);
