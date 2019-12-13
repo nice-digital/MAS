389889dbfa24e4dc54271cfea82d0ca65cbdda5f
Ian Routledge <ian.routledge@nice.org.uk>
2019-12-13 16:15:27 +0000

    MAS-119 Add weekly settings and auto-creation

--- cms/models/Item.js
+++ cms/models/Item.js
@@ -1,13 +1,15 @@
-var keystone = require("keystone");
-var Types = keystone.Field.Types;
+const keystone = require("keystone"),
+	moment = require("moment");
+
+const Types = keystone.Field.Types;
 
 const https = require("https");
-var log4js = require("log4js");
+const log4js = require("log4js");
 
-var logger = log4js.getLogger("Item.js");
+const logger = log4js.getLogger("Item.js");
 logger.level = "debug";
 
-var Item = new keystone.List("Item", {
+const Item = new keystone.List("Item", {
 	map: { name: "title" },
 	autokey: { path: "slug", from: "title", unique: true }
 });
@@ -80,6 +82,7 @@ Item.add({
 		type: Types.Relationship,
 		ref: "Weekly",
 		label: "Weekly newsletter",
+		note: "For use by the NICE MPT only"
 	},
 	// relevancyScore: { 
 	// 	type: Types.Select, 
@@ -121,8 +124,34 @@ Item.schema.pre('validate', function(next) {
     
 });
 
+const createWeeklyIfNeeded = async () => {
+	const sendDateMoment = moment().startOf("isoweek").add(7, "days");
+
+	const WeeklyModel = keystone.list("Weekly").model;
+
+	const weeklyEntity = await WeeklyModel.findOne({
+		sendDate: sendDateMoment.toDate()
+	}).exec();
+
+	if(!weeklyEntity) {
+		const startDateMoment = sendDateMoment.clone().subtract(1, "week"),
+			endDateMoment = startDateMoment.clone().add(4, "days"),
+			dateFormat = "Do MMMM YYYY";
+
+		var newWeekly = new WeeklyModel({
+			title: startDateMoment.format(dateFormat) + " to " + endDateMoment.format(dateFormat),
+			sendDate: sendDateMoment.toDate(),
+			startDate: startDateMoment.toDate(),
+			endDate: endDateMoment.toDate(),
+		});
+		await newWeekly.save();
+	}
+};
+
 // Post save hook to trigger a lambda with the document details
 Item.schema.post("save", async function(doc, next) {
+	await createWeeklyIfNeeded();
+
 	if(!shouldPostLambda){
 		shouldPostLambda = !this.isInitial;
 		next();
@@ -179,8 +208,6 @@ Item.schema.post("save", async function(doc, next) {
 
 	logger.info("...sent PUT request", options);
 });
-
-
 
 Item.defaultColumns = "title, source|20%, specialities|20%";
 Item.register();
--- cms/models/Weekly.js
+++ cms/models/Weekly.js
@@ -2,35 +2,87 @@ var keystone = require("keystone");
 var Types = keystone.Field.Types;
 
 var Weekly = new keystone.List("Weekly", {
-	map: { name: "title" }
+	map: { name: "title" },
+	track: true,
+	nocreate: true,
+	nodelete: true,
+	defaultSort: "-sendDate"
 });
 
 Weekly.add({
-	title: { 
-		type: Types.Text, 
-		required: true, 
-		initial: true,
+	title: {
+		type: Types.Text,
+		required: true,
+		noedit: true
 	},
-	// items: {
-	// 	type: Types.Relationship,
-	// 	ref: "Item",
-	// 	many: true,
-	// 	label: "Daily items",
-	// },
-	weekCommencing: { 
-		type: Types.Datetime, 
-		default: Date.now,
-		label: "Created date",
+	startDate: {
+		type: Types.Date,
+		label: "Start date",
+		noedit: true,
+		hidden: true
 	},
-	createdDate: { 
-		type: Types.Datetime, 
-		default: Date.now,
-		label: "Created date",
+	endDate: {
+		type: Types.Date,
+		label: "End date",
+		noedit: true,
+		hidden: true
 	},
-	commentary: {
+	sendDate: {
+		type: Types.Date,
+		label: "Send date",
+		format: "ddd Do MMM YYYY",
+		noedit: true,
+		note: "The Monday after, when the weekly email is actually sent"
+	},
+	commentaryTitle: {
+		type: Types.Text,
+		label: "Commentary title",
+		note:
+			'Optional.<br>E.g. "New MHRA drug safety advice: September to November 2019"'
+	},
+	commentarySummary: {
 		type: Types.Html,
-		wysiwyg: true
+		wysiwyg: true,
+		height: 200,
+		label: "Commentary summary",
+		note:
+			"Short summary (a few paragraphs) for the weekly's commentary.<br>Required if you've set a commentary title."
+	},
+	commentaryBody: {
+		type: Types.Html,
+		height: 500,
+		label: "Full commentary",
+		note:
+			"The full commentary.<br>Required if you've set a commentary title and summary.",
+		wysiwyg: {
+			overrideToolbar: true,
+			additionalButtons:
+				"styleselect | bold italic | bullist numlist | link | code"
+		}
 	}
+});
+
+Weekly.schema.pre("validate", function(next) {
+	if (this.commentaryTitle && (!this.commentarySummary || !this.commentaryBody))
+		return next(
+			Error(
+				"Commentary summary and body are both required if commentary title is set"
+			)
+		);
+	if (this.commentarySummary && (!this.commentaryTitle || !this.commentaryBody))
+		return next(
+			Error(
+				"Commentary title and body are both required if commentary summary is set"
+			)
+		);
+	if (this.commentaryBody && (!this.commentaryTitle || !this.commentarySummary))
+		return next(
+			Error(
+				"Commentary title and summary are both required if commentary body is set"
+			)
+		);
+
+	next();
 });
 
 Weekly.relationship({
@@ -39,7 +91,5 @@ Weekly.relationship({
 	refPath: "weekly"
 });
 
-
-
-Weekly.defaultColumns = "title";
+Weekly.defaultColumns = "title, sendDate";
 Weekly.register();
