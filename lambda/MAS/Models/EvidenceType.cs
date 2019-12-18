3067de8672283713881a51fa27520e6951770213
Ian Routledge <ian@ediblecode.com>
2019-12-18 11:10:18 +0000

    MAS-148 Add evidence types (#26)
    * MAS-148 Add evidence types
    
    * MAS-148 Make evidence type required
    
    * MAS-148 Update evidence type in API
    
    * MAS-148 Add training content
    
    * MAS-148 Add missing bracket
    
    * MAS-148 Remove training content
    
    * MAS-148 Remove short summary from the item creation popup

---
+++ lambda/MAS/Models/EvidenceType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MAS.Models
+{
+    public class EvidenceType
+    {
+        public string Title { get; set; }
+
+        public string Key { get; set; }
+    }
+}
