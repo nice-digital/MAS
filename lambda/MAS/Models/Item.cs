a7167fee81e2b01d32eb4da60746a5fac0cf6821
Sue Garner <Sue.Garner@nice.org.uk>
2019-12-30 09:01:27 +0000

    MAS-111 Amended Required attribute on properties

--- lambda/MAS/Models/Item.cs
+++ lambda/MAS/Models/Item.cs
@@ -12,13 +12,12 @@ namespace MAS.Models
         [Required, JsonRequired]
         public string Title { get; set; }
         [Required, JsonRequired]
-        public string URL { get; set; }
-        [Required, JsonRequired]
         public Source Source { get; set; }
         [Required, JsonRequired]
-        public string ShortSummary { get; set; }
         public EvidenceType EvidenceType { get; set; }
+        public string ShortSummary { get; set; }
         public string UKMiComment { get; set; }
         public string ResourceLinks { get; set; }
+        public string URL { get; set; }
     }
 }
