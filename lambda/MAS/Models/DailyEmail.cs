756da19bdc0cf7b936b20153e81d4ca0b7337b74
dylan <dylan.riley@nice.org.uk>
2020-01-03 12:47:48 +0000

    MAS-129 Add speciality field to MAD

--- lambda/MAS/Models/DailyEmail.cs
+++ lambda/MAS/Models/DailyEmail.cs
@@ -9,13 +9,15 @@ namespace MAS.Models
     public class DailyEmail
     {
         public List<Item> Items { get; set; }
-        public List<IGrouping<EvidenceType,Item>> GroupedItems
+
+        public List<IGrouping<EvidenceType, Item>> GroupedItems
         {
             get
             {
                 return Items.GroupBy(x => x.EvidenceType).ToList();
             }
         }
+
 
         public string HTML
         {
@@ -25,20 +27,27 @@ namespace MAS.Models
                 
                 foreach (var group in GroupedItems)
                 {
-                    var speciality = group.Single().EvidenceType.Title;
-                    body.Append("<strong>" + speciality + "</strong>");
+                    var evidenceType = group.Single().EvidenceType.Title;
+
+                    body.Append("<div class='evidenceType'>");
+                    body.Append("<strong>" + evidenceType + "</strong>");
 
                     foreach(var item in group)
                     {
+                        body.Append("<div class='item'>");
+                        body.Append(item.Title);
                         body.Append("<br>");
                         body.Append(item.Source.Title);
                         body.Append("<br>");
-                        body.Append(item.Title);
+                        body.Append(String.Join(" | ", item.Speciality.Select(x => x.Title)));
                         body.Append("<br>");
                         body.Append(item.ShortSummary);
                         body.Append("<br>");
                         body.Append("<a href='https://www.medicinesresources.nhs.uk/" + @item.Slug + "'>SPS Comment</a>");
+                        body.Append("</div>");
                     }
+
+                    body.Append("</div>");
                 }
 
                 return body.ToString();
