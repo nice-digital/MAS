eb4cb2a48bfdbc2d41107acdf097425ca1745437
Sue Garner <Sue.Garner@nice.org.uk>
2020-01-06 10:53:36 +0000

    MAS-150 Added Weekly model

---
+++ lambda/MAS/Models/Weekly.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MAS.Models
+{
+    public class Weekly
+    {
+        public string Title { get; set; }
+        public DateTime SendDate { get; set; }
+        public string CommentaryTitle { get; set; }
+        public string CommentarySummary { get; set; }
+        public string FullCommentary { get; set; }
+    }
+}
