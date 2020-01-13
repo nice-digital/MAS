ec0b20f75192e3c8b455cdaae11eaf1f261aa56a
lizaadebowale <liza.adebowale@nice.org.uk>
2020-01-13 09:30:56 +0000

    MAS-139 - Automated test - Add new item to CMS (#39)
    * automate add new and edit item to CMS
    
    * removed console log lines initially added
    
    * changed the run.sh file
    
    * scoped out accessibility test step
    
    * MAS-165 SPS training mop up
    
    * more changes
    
    * MAS-165 Port changed back
    
    * MAS-139 Fix linting error
    
    Co-authored-by: Eleanor Mollett <eleanor.mollett@gmail.com>
    Co-authored-by: Ian Routledge <ian@ediblecode.com>

--- functional-tests/steps/then.js
+++ functional-tests/steps/then.js
@@ -1,6 +1,10 @@
 import "@nice-digital/wdio-cucumber-steps/lib/then";
 import { Then } from "cucumber";
 import checkContainsText from "@nice-digital/wdio-cucumber-steps/lib/support/check/checkContainsText";
+import validateRecordForm from "../support/check/validate";
+import setInputField from "@nice-digital/wdio-cucumber-steps/lib/support/action/setInputField";
+import { addUrl, addPublicationDate, addEvidenceType, addSpsComment, addShortSummary, addResourceLink } from "../support/action/addInput";
+import { selectEvidenceType, selectSpecialities, relevancy } from "../support/action/click";
 
 Then(
     /^I expect the page to contain the text "([^"]*)?"$/, (text) => {
@@ -10,14 +14,89 @@ Then(
                 value = "//DIV[@class='auth-footer']"
                 break;
             case "MAS":
-                value = "//DIV[@class='dashboard-heading'][text()='MAS']"
+                value = "//DIV[@class='dashboard-heading'][text()='Medicines Awareness Service']"
                 break;
             case "You have been signed out.":
-                value = "//DIV[@class='alert_1wamaxc-o_O-info_1lss0us']"
-                break;             
+                value = "[data-alert-type='info']";
+                break; 
+            case "Your changes have been saved successfully":
+                value = "[data-alert-type='success']";
+                break;          
             default:
                 value = "div.dashboard-header";
           }
           checkContainsText("element", value, false, text);
     }
+);
+Then(
+    /^I expect to see a list of items$/, () => {
+        var table = ("table.Table.ItemList");
+        browser.isExisting(table);
+        browser.pause(2000);
+    }          
+);
+Then(
+    /^I expect a create a new item form pops up$/, () => {
+        var form = ("[data-screen-id='modal-dialog']");
+        browser.isExisting(form);
+        browser.getValue(form);
+    }          
+);
+Then(
+    /^I can add an Evidence type$/, () => { 
+        browser.click("[for='evidenceType'] .Select-control .Select-placeholder");    
+        browser.waitForVisible("[for='evidenceType'] .Select-menu ", 5000);
+        browser.click("[for='evidenceType'] .Select-menu .Select-option#react-select-3--option-3");
+        browser.pause(5000);
+    }          
+);
+Then(
+    /^I expect the new record is added to the list of items$/, () => {
+        browser.pause(3000);
+        browser.click("[data-list-path='items'] a");
+        browser.waitForVisible("//A[text()='NSAIDs Automated test again']");      
+    }          
+);
+Then(
+    /^I expect the record form is visible$/, 
+    validateRecordForm
+);
+Then(
+    /^I can add a URL$/, 
+    addUrl
+);
+Then(
+    /^I can add a Short summary$/, 
+    addShortSummary
+);
+Then(
+    /^I can add a Publication date$/, 
+    addPublicationDate
+);
+Then(
+    /^I can add a Speciality type$/, () => {  
+        var speciality = $ ("[for='speciality'] .Select-control .Select-placeholder");
+        speciality.scroll(250, 250);
+        browser.pause(5000);
+        browser.click("[for='speciality'] .Select-control .Select-arrow");
+        browser.click("[for='speciality'] .Select-menu .Select-option#react-select-4--option-4")
+              
+    }                
+);
+Then(
+    /^I can add a SPS comment$/, 
+    addSpsComment
+);
+Then(
+    /^I can add a Resource link$/, 
+    addResourceLink
+);
+Then(
+    /^I can add a Weekly relevancy score$/, () => {  
+        var relevancy = $ ("[for='relevancy'] .Select-control .Select-placeholder");
+        relevancy.scroll(950, 750);
+        browser.click("[for='relevancy'] .Select-control .Select-arrow");
+        browser.click("[for='relevancy'] .Select-menu .Select-option#react-select-5--option-0")
+            
+    }                
 );
