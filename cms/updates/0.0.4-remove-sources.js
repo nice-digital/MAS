e7b119f3c9f13339a5cd0b808b5d971a64c88106
Eleanor Mollett <eleanor.mollett@gmail.com>
2020-01-23 15:14:30 +0000

    MAS-154 Linting and improvements

--- cms/updates/0.0.4-remove-sources.js
+++ cms/updates/0.0.4-remove-sources.js
@@ -1,6 +1,6 @@
 const keystone = require("keystone"),
 	async = require("async"),
-	log4js = require("log4js");
+	log4js = require("log4js"),
 	newSources = require("./sources2.json");
 
 const Source = keystone.list("Source");
@@ -8,35 +8,29 @@ const Source = keystone.list("Source");
 const logger = log4js.getLogger();
 
 const removeSource = async (source, done) => {
-	const { title, _id } = source;
+	const { title, _id, oldEPiServerId } = source;
 
-	const existsInNewSourceList = newSources.some(element => element.oldEPiServerId == source.oldEPiServerId);
+	const existsInNewSourceList = newSources.some(
+		element => element.oldEPiServerId == oldEPiServerId
+	);
 
-	if(existsInNewSourceList){
-		logger.debug(`Not deleting ${title} as it exists in the new source list`)
-		done();
-		return;
+	if (existsInNewSourceList) {
+		logger.debug(`Not deleting ${title} as it exists in the new source list`);
+		return done();
 	}
-	
+
 	logger.debug(
 		`Deleting source ${title} as it doesn't exist in the new source list`
 	);
 
 	try {
-		await Source.model.findById(_id)
-				.remove(function(err) {
-					if(err){
-						logger.error(`Error deleting ${title}`, err);	
-					}else{
-						logger.info(`Successfully deleted ${title}`)
-					}
-				});
+		await Source.model.findById(_id).remove();
+		logger.info(`Successfully deleted ${title}`);
+		done();
 	} catch (err) {
 		logger.error(`Error deleting ${title}`, err);
-		throw err;
+		done(err);
 	}
-	
-	done();
 };
 
 exports = module.exports = async function(done) {
@@ -44,21 +38,13 @@ exports = module.exports = async function(done) {
 
 	let existingSources;
 	try {
-		existingSources = await Source.model
-		.find()
-		.exec();
+		existingSources = await Source.model.find().exec();
 	} catch (err) {
 		logger.error(`Error fetching sources with entities in update`, err);
 		throw err;
 	}
 
-	logger.debug(
-		`Found ${existingSources.length} sources to update`
-	);
+	logger.debug(`Found ${existingSources.length} sources to update`);
 
-	async.forEach(
-		existingSources,
-		removeSource,
-		done
-	);
+	async.forEach(existingSources, removeSource, done);
 };
