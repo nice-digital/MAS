087e8989948f7f8160bb19f04498f0f932355365
Eleanor Mollett <eleanor.mollett@gmail.com>
2020-01-24 09:52:49 +0000

    MAS-204 Change title to source name (#57)

--- cms/models/Source.js
+++ cms/models/Source.js
@@ -7,7 +7,7 @@ var Source = new keystone.List("Source", {
 });
 
 Source.add({
-	title: { type: String, required: true, initial: true },
+	title: { type: String, required: true, initial: true, label: "Source name" },
 	oldEPiServerId: {
 		label: "Old EPiServer ID",
 		type: Types.Number,
