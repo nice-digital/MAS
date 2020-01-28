6c305abdda7c9191c2a55c0256744b84ac49c6af
Ian Routledge <ian.routledge@nice.org.uk>
2020-01-28 17:14:44 +0000

    MAS-163 Add some logging

--- lambda/MAS/LambdaEntryPoint.cs
+++ lambda/MAS/LambdaEntryPoint.cs
@@ -5,6 +5,11 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.AspNetCoreServer.Internal;
+using Amazon.Lambda.Core;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MAS
 {
@@ -19,6 +24,15 @@ namespace MAS
         // the base class to Amazon.Lambda.AspNetCoreServer.ApplicationLoadBalancerFunction
         Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
     {
+        private ILogger<LambdaEntryPoint> _logger;
+
+        protected override void PostCreateWebHost(IWebHost webHost)
+        {
+            _logger = webHost.Services.GetRequiredService<ILogger<LambdaEntryPoint>>();
+
+            base.PostCreateWebHost(webHost);
+        }
+
         /// <summary>
         /// The builder has configuration, logging and Amazon API Gateway already configured. The startup class
         /// needs to be configured in this method using the UseStartup<>() method.
@@ -28,6 +42,26 @@ namespace MAS
         {
             builder
                 .UseStartup<Startup>();
+        }
+
+        public override Task<APIGatewayProxyResponse> FunctionHandlerAsync(APIGatewayProxyRequest request, ILambdaContext lambdaContext)
+        {
+            _logger.LogCritical("FunctionHandlerAsync");
+            _logger.LogCritical("request.QueryStringParameters ", request.QueryStringParameters);
+            _logger.LogCritical("request.PathParameters ", request.PathParameters);
+            _logger.LogCritical("request.Resource ", request.Resource);
+            _logger.LogCritical("request.Body ", request.Body);
+            _logger.LogCritical("", request);
+            _logger.LogCritical("request.Path", request.Path);
+            _logger.LogCritical("request.RequestContext.Path", request.RequestContext.Path);
+            _logger.LogCritical("request.RequestContext.ResourcePath", request.RequestContext.ResourcePath);
+
+            //if (request.Resource == "WarmingLambda")
+            //{
+
+            //}
+
+            return base.FunctionHandlerAsync(request, lambdaContext);
         }
     }
 }
