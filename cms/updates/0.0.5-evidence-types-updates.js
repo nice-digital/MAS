22bbc2537457ea85d220e4dfb161ffc0718467a2
dylan <dylan.riley@nice.org.uk>
2020-02-13 18:32:06 +0000

    MAS-206 Rewrite updates
    Rewrite updates to use jsonld from topbraid

--- cms/updates/0.0.5-evidence-types-updates.js
+++ cms/updates/0.0.5-evidence-types-updates.js
@@ -1,16 +1,18 @@
 const keystone = require("keystone"),
 	async = require("async"),
 	log4js = require("log4js"),
-	migrationList = require("./evidence-types-update-list.js");
+	fs = require("fs");
+
 
 const logger = log4js.getLogger();
-const EvidenceTypeList = keystone.list("EvidenceType");
+const evidenceTypeList = keystone.list("EvidenceType");
 
-const deleteEvidenceType = async (etitle, done) => {
+const deleteEvidenceType = async (evidenceType, done) => {
+	let eTitle = evidenceType.title;
 	logger.info(`Deleting evidence type: ${etitle}`);
 
 	try {
-		await EvidenceTypeList.model.findOne({ title: { "$regex": etitle, "$options": "i" } }).remove();
+		await evidenceTypeList.model.findOne({ title: { "$regex": etitle, "$options": "i" } }).remove();
 		done();
 	} catch (err) {
 		logger.error(`Error deleting ${etitle}`, err);
@@ -21,26 +23,27 @@ const deleteEvidenceType = async (etitle, done) => {
 };
 
 const renameEvidenceType = async (evidenceType, done) => {
-	const {
-		oldTitle,
-		newTitle
-	} = evidenceType;
+	let evidenceTypeInKeystone = evidenceTypeList.model.findOne({key: evidenceType["@id"] });
+	let newTitle = evidenceType.prefLable["@value"];
+	let oldTitle = evidenceTypeInKeystone.title;
 
-	logger.info(`Renaming evidence type ${oldTitle} to ${newTitle}`);
+	if(newTitle != evidenceTypeInKeystone.title){
+		logger.info(`Renaming evidence type ${oldTitle} to ${newTitle}`);
 
-	try {
-		await EvidenceTypeList.model.updateOne(
-			{ title: { "$regex": oldTitle, "$options": "i" } },
-			{ $set: { title: newTitle } }
-		);
-		done();
-	} catch (err) {
-		logger.error(`Error renaming ${oldTitle} to ${newTitle}`, err);
-		done(err);
-	}
-
-	logger.info(`Successfully renamed ${oldTitle} to ${newTitle}`);
-};
+		try {
+			await evidenceTypeList.model.updateOne(
+				{ key: evidenceType.id},
+				{ $set: { title: newTitle } }
+			);
+			done();
+		} catch (err) {
+			logger.error(`Error renaming ${oldTitle} to ${newTitle}`, err);
+			done(err);
+		}
+	
+		logger.info(`Successfully renamed ${oldTitle} to ${newTitle}`);
+	}	
+}
 
 const addNewEvidenceType = async (evidenceType, done) => {
 	const {
@@ -50,7 +53,7 @@ const addNewEvidenceType = async (evidenceType, done) => {
 
 	logger.info(`Adding new evidence type: ${title}`);
 
-	const newEvidenceTypeDbModel = new EvidenceTypeList.model({
+	const newEvidenceTypeDbModel = new evidenceTypeList.model({
 		title: title,
 		broaderTitle: broader,
 		key: encodeURIComponent(title)
@@ -69,24 +72,36 @@ const addNewEvidenceType = async (evidenceType, done) => {
 
 exports = module.exports = function (done) {
 	logger.info(`Started update 05.`);
-	const {
-		evidenceTypesTodelete,
-		evidenceTypesToRename,
-		newEvidenceTypes
-	} = migrationList();
 
-	let callback = function (err) {
-		if (err != null) {
-			logger.error(err);
+	fs.readFile(
+		path.resolve(__dirname, "0.0.5-evidence-types-update.jsonld"),
+		"UTF-8",
+		function(err, contents){
+			const evidenceTypesJsonLd = JSON.parse(contents),
+				latestEvidenceTypes = evidenceTypesJsonLd["@graph"];
+			
+			let callback = function (err) {
+				if (err != null) {
+					logger.error(err);
+				}
+			}
+			
+			let latestEvidenceTypesIds = latestEvidenceTypes.map(a => a["@id"]);
+			let evidenceTypesToDelete = evidenceTypeList.find( { key: { $nin: latestEvidenceTypesIds } } );
+			let deletes = async.forEach(evidenceTypesTodelete, deleteEvidenceType, callback);
+			
+			let latestEvidenceTypesNames = content.map(a => a.prefLabel["@value"]);
+			evidenceTypeList.find( { key: { $in: latestEvidenceTypesIds }, title: { $ne: latestEvidenceTypesNames } } );
+			let updates = async.forEach(evidenceTypesTodelete, renameEvidenceType, callback); 
+
+			let newEvidenceTypes = content.map(a => !latestEvidenceTypesNames.includes(a.prefLabel["@value"]));
+			evidenceTypeList.find( { key: { $in: latestEvidenceTypesIds }, title: { $ne: latestEvidenceTypesNames } } );
+			let adds = async.forEach(newEvidenceTypes, addNewEvidenceType, callback); 
+
+			Promise.all([deletes, updates, adds]).then(() => {
+				logger.info(`Finished update 05.`);
+				done();
+			});
 		}
-	}
-
-	let deletes = async.forEach(evidenceTypesTodelete, deleteEvidenceType, callback);
-	let updates = async.forEach(evidenceTypesToRename, renameEvidenceType, callback);
-	let adds = async.forEach(newEvidenceTypes, addNewEvidenceType, callback);
-
-	Promise.all([deletes, updates, adds]).then(() => {
-		logger.info(`Finished update 05.`);
-		done();
-	});
+	);
 };
