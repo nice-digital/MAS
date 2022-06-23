369075cadb59001ac9f04141ab6dbb483cdbe4e2
imranazad <imran.azad@nice.org.uk>
2022-06-23 20:38:31 +0100

    MAS-248 Add Logging

--- cms/overridden_files/lib/core/initExpressSession.js
+++ cms/overridden_files/lib/core/initExpressSession.js
@@ -4,8 +4,13 @@ var cookieParser = require("cookie-parser");
 var debug = require("debug")("keystone:core:initExpressSession");
 var Promise = require("es6-promise").Promise;
 var safeRequire = require("../../../node_modules/keystone/lib/safeRequire");
+log4js = require("log4js");
+
+const logger = log4js.getLogger("Item.js");
 
 module.exports = function initExpressSession(mongoose) {
+	logger.error("hello*****************");
+		
 	if (this.expressSession) return this;
 
 	var sessionStorePromise;
